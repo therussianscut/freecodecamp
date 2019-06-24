a2a605705950900f4271d0ef8eebb4ec8df15f51
Sean McCann <smccann@athium.com>
2019-06-24 09:59:15 +0100

    First upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    <div class="col-3 p-5">
+        <img src="https://scontent-lhr3-1.cdninstagram.com/vp/e8a9c5ac05e3255e8c11901972a5185e/5DA14CC8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com" class="rounded-circle"
+        style="max-height: 100px">
+
+    </div>
+
+
+    <div class="col-9 pt-5">
+        <div> <h1> FreeCodeCamp</h1></div>
+        <div class="d-flex">
+            <div class="pr-3"><strong> 150</strong> Posts </div>
+            <div class="pr-3"><strong> 23K</strong> Followers </div>
+            <div class="pr-3"><strong> 212</strong> Following </div>
+
+
+
+        </div>
+
+    </div>
+
+    </div>
+
+</div>
+@endsection
